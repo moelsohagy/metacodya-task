ddf5e2d27f57abfed40912c88c4b2c8328707755
Mohamed Elsohagy <m.elsohagy@dashcode.co>
2024-09-01 00:31:45 +0300

    init

---
+++ components/ProductCard.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function productCard({ product }) {
+  return (
+    <Link
+      key={product.id}
+      className={styles.product}
+      href={`/product/${product.id}`}
+    >
+      <div>
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={1024}
+          height={1024}
+        />
+        <h3>{product.title}</h3>
+      </div>
+
+      <p className={styles.price}>${product.price}</p>
+    </Link>
+  )
+}
---
+++ components/ProductInfo.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import styles from '../styles/Product.module.css'
+
+export default function ProductInfo({ product }) {
+  return (
+    <div className={styles.productDetails}>
+      <div>
+        <h1>{product.title}</h1>
+        <div className={styles.category}>
+          <h3>{product.category}</h3>
+          <span className={styles.price}>${product.price}</span>
+        </div>
+        <p className={styles.description}>{product.description}</p>
+      </div>
+
+      <Image
+        src={product.image}
+        alt={product.title}
+        width={1024}
+        height={1024}
+      />
+    </div>
+  )
+}
